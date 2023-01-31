6ca73c083a92840f37b9ce57a1dee5bb2adcf935
Jake <jake.gloudemans@gmail.com>
2023-01-31 13:06:40 -0500

    General cleanup

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 
-let topicText = {};
-
-let topicList = [
-    "intro",
+let topicText = {};  // Var to store text blurbs so we can switch between them quickly
+let topicList = [    // List of topic names to iterate through when arrows are pressed
+    "intro",         // TODO this should be generated programmatically from JSON
     "liberalism",
     "economy",
     "housing",
@@ -11,11 +10,13 @@ let topicList = [
     "pragmatism",
     "tolerance"
 ]
+let currentTopic = "intro"; // Var to keep track of which topic is currently active
 
-let currentTopic = "intro";
 
 /**
- * This function is called when the page finishes loading
+ * This function is called when the page finishes loading. Any functions required
+ * for loading/rendering the page should be added here, and all default event listeners
+ * should be attached here
  */
 $(document).ready(function() {
 
@@ -34,12 +35,11 @@ $(document).ready(function() {
 });
 
 
-
 /**
  * This method fetches a JSON object containing all the text blurbs for populating
  * various elements on the page. It retrieves the JSON, parses any special markers
- * in the contents (e.g. creating links embedded in the text), and updates the
- * appropriate elements on the page.
+ * in the contents (e.g. creating links embedded in the text, separating paragraphs)
+ * and updates the appropriate elements on the page.
  */
 function populateBlurbs() {
 
@@ -74,11 +74,13 @@ function populateBlurbs() {
                 text = text.replace("[[P]]", startTag);
             }
 
-            // Update the html contents for the specified id
+            // Store a dictionary mapping from topic names to text descriptions
+            // for fast retrieval when active topic is changed
             topicText[id] = text;
 
         }
 
+        // Set the active topic to the "intro" message
         $(".main-text").html(topicText["intro"]);
 
     });
@@ -113,39 +115,17 @@ function populateFooter() {
             $("#footer-contents").append(section);
 
         }
+
     });
 
 }
 
 
-function populateEvents() {
-
-    // Fetch footer headings and links from server
-    let prefix = window.location.hostname === "localhost" ? "" : window.location.href;
-    $.getJSON(prefix + "data/events.json", function(data) {
-
-        // Iterate over headings
-        for (const event in data) {
-
-            // Add a new vertical for each heading, create the heading inside it
-            let section = $("<div>").addClass("footer-vertical");
-            section.append($("<h2>").addClass("footer-vertical-heading").html(heading));
-
-            // Add the list of links under the heading
-            for (const link in data[heading]) {
-                let linkTag = $("<a>").attr("href", data[heading][link]).attr("target", "_blank").html(link);
-                section.append(linkTag)
-            }
-
-            // Add the heading and links to the page
-            $("#footer-contents").append(section);
-
-        }
-    });
-
-}
-
-
+/**
+ * This function updates the text on the screen and highlights the new active topic
+ * whenever the topic changes. If the selected topic was already active, it should
+ * have no effect.
+ */
 function handleTopicClick() {
 
     if (!$(this).hasClass("selected")) {
@@ -162,16 +142,22 @@ function handleTopicClick() {
 
         // Update the text heading
         if (topicName === "intro") {
-            $("#main-text-heading").html("About Us");
+            $("#main-text-title").html("About Us");
         } else {
-            let emphasized = "<span class='main-text-heading-em'>" + topicName + "</span>";
-            $("#main-text-heading").html("Our Philosophy: " + emphasized);
+            let emphasized = "<span class='main-text-title-em'>" + topicName + "</span>";
+            $("#main-text-title").html("Our Philosophy: " + emphasized);
         }
 
     }
 }
 
 
+/**
+ * This function advances the topic (either forwards or backwards), when one of
+ * the arrow buttons is pressed. These buttons are used on narrow screens to change
+ * the topics, instead of the topic selectors that are visible on wider screens. It works
+ * by simulating a press of the topic selector button for the next topic in order
+ */
 function handleArrowClick() {
 
     // Determine which direction to move in
